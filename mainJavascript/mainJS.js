7e1cae22a22a7d20dc0dbd9d50d812f1f71f5ff7
David Boucher <david_boucher@outlook.com>
2016-09-05 15:51:19 -0700

    Homepage
    Created a nav bar, which loads in different projects depending what is
    clicked on. Hoping that these are loaded correctly. Projects will have
    to be worked on for content to actually appear.

--- mainJavascript/mainJS.js
+++ mainJavascript/mainJS.js
@@ -1,7 +1,24 @@
-
-$.ajax({
-	url: "./ticTacToe/ticTacToe.html"
-}).done(function(data){
-	console.log(data);
-	$("#projectContent").html(data);
+// Load different javascript creations based upon what nav element the user clicks on.
+$("#menu-bar li").on("click", function()
+{
+	var selected = $(this).attr('id');
+	var url = "this should be logged";
+		switch (selected) {
+		case "homePage":
+		break;
+		case "TTT":
+		var url = "../ticTacToe/ticTacToe.html";
+		break;
+		case "Pong":
+		var url = "../pong/pong.html";
+		break;
+		case "footballScores":
+		var url = "../footballScores/footballScores.html";
+		break;
+	};
+	$.ajax({
+		url: url
+	}).done(function(data){
+		$("#projectContent").html(data);
+	});
 });
