4767719e68a7974c084e22be07e9c6ebbe528473
David Boucher <david_boucher@outlook.com>
2016-09-05 16:09:00 -0700

    Another fix

--- mainJavascript/mainJS.js
+++ mainJavascript/mainJS.js
@@ -3,19 +3,21 @@ console.log(window.location.href);
 // Load different javascript creations based upon what nav element the user clicks on.
 $("#menu-bar li").on("click", function()
 {
+	$("#menu-bar li").removeClass('unClicked');
 	var selected = $(this).attr('id');
+	$(this).addClass('Clicked');
 	var url = "this should be logged";
 		switch (selected) {
 		case "homePage":
 		break;
 		case "TTT":
-		var url = "../ticTacToe/ticTacToe.html";
+		var url = "ticTacToe/ticTacToe.html";
 		break;
 		case "Pong":
-		var url = "../pong/pong.html";
+		var url = "pong/pong.html";
 		break;
 		case "footballScores":
-		var url = "../footballScores/footballScores.html";
+		var url = "footballScores/footballScores.html";
 		break;
 	};
 	$.ajax({
